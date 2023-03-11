a34fdea82b86c3de11c0d196c524739e504886ac
utubun <dmytro.strunin@gmail.com>
2023-03-11 23:23:19 +0100

    Initial commit

---
+++ R/zzz.R
@@ -0,0 +1,45 @@
+###
+###
+
+.pkgname <- "BSgenome.Paeruginosa.NCBI.PA14"
+
+.seqnames <- "NC_008463.1"
+
+.circ_seqs <- character(0)
+
+.mseqnames <- NULL
+
+.onLoad <- function(libname, pkgname)
+{
+    if (pkgname != .pkgname)
+        stop("package name (", pkgname, ") is not ",
+             "the expected name (", .pkgname, ")")
+    extdata_dirpath <- system.file("extdata", package=pkgname,
+                                   lib.loc=libname, mustWork=TRUE)
+
+    ## Make and export BSgenome object.
+    bsgenome <- BSgenome(
+        organism="Pseudomonas aeruginosa",
+        common_name="P. aeruginosa",
+        genome="ASM1462v1",
+        provider="NCBI",
+        release_date="2006-10-06",
+        source_url="https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/014/625/GCF_000014625.1_ASM1462v1/GCF_000014625.1_ASM1462v1_genomic.fna.gz",
+        seqnames=.seqnames,
+        circ_seqs=.circ_seqs,
+        mseqnames=.mseqnames,
+        seqs_pkgname=pkgname,
+        seqs_dirpath=extdata_dirpath
+    )
+
+    ns <- asNamespace(pkgname)
+
+    objname <- pkgname
+    assign(objname, bsgenome, envir=ns)
+    namespaceExport(ns, objname)
+
+    old_objname <- "Paeruginosa"
+    assign(old_objname, bsgenome, envir=ns)
+    namespaceExport(ns, old_objname)
+}
+
---
+++ README.md
@@ -0,0 +1,47 @@
+
+# BSgenome.Paeruginosa.NCBI.PA14
+
+<!-- badges: start -->
+[![Lifecycle: experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://lifecycle.r-lib.org/articles/stages.html#experimental)
+<!-- badges: end -->
+
+The package provides a full genome assembly of E. coli str. K12, substr. MG1655. Data source: [NCBI](https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/014/625/GCF_000014625.1_ASM1462v1/).
+
+## Installation
+
+You can install the development version of BSgenome.Paeruginosa.NCBI.PA14 from [GitHub](github.com) with:
+
+``` r
+devtools::install_github('utubun/BSgenome.Paeruginosa.NCBI.PA14')
+```
+
+## Example
+
+Load *P. aeruginosa* str. PA14 genome, and assign it ot the variable `genome`:
+
+``` r
+library(BSgenome.Paeruginosa.NCBI.PA14)
+
+genome <- BSgenome.Paeruginosa.NCBI.PA14
+```
+
+Get the information about the genome:
+
+``` r
+sprintf(
+  'Genome:\t%s\nName:\t%s\nOrganism:\t%s\nProvider:\t%s\nReleased:\t%s',
+  sapply(c(bsgenomeName, commonName, organism, provider, releaseDate), \(f) {f(genome)})
+)
+```
+
+List the methods available for the given object class:
+
+```r
+methods(class = class(genome))
+```
+
+# More examples
+
+``` r
+help(package = 'BSgenome')
+```
